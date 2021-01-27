623715d6ad6e048ce0e5d94d45d29c4024581757
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-01-27 17:24:05 +0000

    install.sh will create basic testfile

--- install.sh
+++ install.sh
@@ -5,6 +5,18 @@
 #cd $phoronix
 #./install-sh
 
+
+#create basic testfile if it does not exist
+if [ ! -d "./testfile" ] ; then
+  mkdir './testfile'
+  touch './testfile/testfile'
+  echo "#Basic system test" > $testfile
+  echo "SciMark" >> $testfile
+  echo "Himeno Benchmark" >> $testfile
+  echo "Flexible IO Tester" >> $testfile
+  echo "RAMspeed SMP" >> $testfile
+fi
+
 while IFS= read -r line
 do
   if  [[ $line == '#'* ]]; then

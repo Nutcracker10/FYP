928fc4af88fdebf728c2680eeb40d10ab45e31ea
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-01-31 17:19:15 +0000

    beginning to design run process

--- run.sh
+++ run.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-. ./vars.sh
 if [[ $(id -u) -ne 0 ]]; then
   echo "ERROR: run.sh must be run as root"
   exit 1
 fi
 
-./install.sh
-
-
+while getopts ":hqrt" opt; do
+  case ${opt} in 
+    h ) # Help
+      ;;
+    q ) # Quick
+      ;;
+    r ) # run tests
+      ;;
+    t ) # Edit testfile
+      ;;
+    \?)
+      echo "Usage: run [-h] [-q] [-r] [-t]"
+      ;;
+  esac
+done

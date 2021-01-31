40bf2b3d262efaca1584341f20616359163254ff
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-01-31 17:18:37 +0000

    cleaned up files

---
+++ scripts/cleanup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+. ./vars
+
+while IFS= read -r line
+do
+  if  [[ $line == '#'* ]]; then
+    continue
+  fi
+  yes | phoronix-test-suite remove-installed-test $line
+done < $testfile
+
---
+++ scripts/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. ./vars
+
+cd $phoronix
+./install-sh
+
+
+while IFS= read -r line
+do
+  if  [[ $line == '#'* ]]; then
+    continue
+  fi
+  yes | phoronix-test-suite install-test $line
+done < $testfile

04a1927b3745dad6cf972c57cf53980f299b97bf
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-02-01 12:12:50 +0000

    added a list ability to run.sh

--- run.sh
+++ run.sh
@@ -5,18 +5,32 @@ if [[ $(id -u) -ne 0 ]]; then
   exit 1
 fi
 
-while getopts ":hqrt" opt; do
+while getopts ":hlqrt" opt; do
   case ${opt} in 
     h ) # Help
+      echo "help"
       ;;
+
+    l ) #List all tests
+      phoronix-test-suite list-all-tests
+      ;;
+    
     q ) # Quick
       ;;
+    
     r ) # run tests
       ;;
+    
     t ) # Edit testfile
       ;;
+
     \?)
       echo "Usage: run [-h] [-q] [-r] [-t]"
       ;;
   esac
 done
+
+if (( $OPTIND == 1 )) ; then
+  ./scripts/testfile_generate.sh
+  ./scripts/install.sh
+fi
--- vars.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-projectRoot='./'
-phoronix='./benchmarks/phoronix-test-suite/'
-testfile='./testfile/testfile'

7433992ddf70f524c5fa844655fa40c0f9988de8
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-03-22 16:19:11 +0000

    optimize now works for N dimensions

--- scripts/optimise.py
+++ scripts/optimise.py
@@ -5,29 +5,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import oapackage
 
-data = np.random.rand(2, 50)
+data = np.random.rand(4, 50)
 
 def find_pareto(points):
-  pareto=oapackage.ParetoDoubleLong()
-  for ii in range(0, points.shape[1]):
-    w=oapackage.doubleVector( (points[0,ii], points[1,ii]) )
-    pareto.addvalue(w, ii)
+  points = points[points.sum(1).argsort()[::1]]
+  undominated = np.ones(points.shape[0], dtype=bool)
 
-  pareto.show(verbose=1)
-  # Plot results
-  lst=pareto.allindices() # the indices of the Pareto optimal designs
+  for i in range(points.shape[0]):
+    n = points.shape[0]
 
-  optimal_points=points[:,lst]
+    if i >= n: 
+      break
 
-  h=plt.plot(points[0,:], points[1,:], '.b', markersize=16, label='Non Pareto-optimal')
-  hp=plt.plot(optimal_points[0,:], optimal_points[1,:], '.r', markersize=16, label='Pareto optimal')
-  plt.xlabel('Objective 1', fontsize=16)
-  plt.ylabel('Objective 2', fontsize=16)
-  plt.xticks([])
-  plt.yticks([])
-  _=plt.legend(loc=3, numpoints=1)
+    undominated[i+1:n] = (points[i+1:] >= points[i]).any(1)
+    points = points[undominated[:n]]
+
+  return points
+  
+
+#def find_pareto(points):
+#  pareto=oapackage.ParetoDoubleLong()
+#  for ii in range(0, points.shape[1]):
+#    w=oapackage.doubleVector( (points[0,ii], points[1,ii]) )
+#    pareto.addvalue(w, ii)
+#
+#  pareto.show(verbose=1)
+#  # Plot results
+#  lst=pareto.allindices() # the indices of the Pareto optimal designs
+#
+#  optimal_points=points[:,lst]
+#
+#  h=plt.plot(points[0,:], points[1,:], '.b', markersize=16, label='Non Pareto-optimal')
+#  hp=plt.plot(optimal_points[0,:], optimal_points[1,:], '.r', markersize=16, label='Pareto optimal')
+#  plt.xlabel('Objective 1', fontsize=16)
+#  plt.ylabel('Objective 2', fontsize=16)
+#  plt.xticks([])
+#  plt.yticks([])
+#  _=plt.legend(loc=3, numpoints=1)
+
+points = find_pareto(data)
+print(points)
 
 
-#analyse_points(data)
-find_pareto(data)
-plt.show()
+#plt.show()

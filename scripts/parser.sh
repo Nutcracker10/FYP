37144ad7df2987d6d2c80923fd0de18635f70757
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-03-26 17:08:18 +0000

    test results now are parsed into one csv

--- scripts/parser.sh
+++ scripts/parser.sh
@@ -6,15 +6,20 @@ phoronix-test-suite result-file-to-csv $result
 
 file=$(ls -Art /home/${SUDO_USER:-${USER}}/*.csv | tail -n 1)
 
+#Pre-process the csv
+sed -i -e '1,24d' $file
+cut --complement -f 1-2 -d, $file >> "./results/results.csv"
+
 #mv /home/${SUDO_USER:-${USER}}/*.csv "./results/"
 mv $file "./results/"
 echo "Result moved to results/"
 
 #remove unnecessary details
-for f in "./results/"
-do
-  sed -i -e '1,24d' f
-done
+#for f in "./results/"
+#do
+#  echo $f
+#  sed -i -e '1,24d' $f
+#  cut --complement -f 1-2 -d, $f >> results.csv
+#done
 
-#cut --complement -f 1-2 -d, test3.csv
 

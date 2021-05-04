f9f64c592de175443304986bf23c8e6cb327ffdc
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-05-04 19:34:16 +0100

    Removed unnessecary comments

--- scripts/parser.sh
+++ scripts/parser.sh
@@ -2,6 +2,10 @@
 
 result=$(basename  "$(ls -t "/home/${SUDO_USER:-${USER}}/.phoronix-test-suite/test-results/" | head -1)" )
 
+if [ ! -e "./results" ];then 
+	mkdir "./results"
+fi
+
 phoronix-test-suite result-file-to-csv $result
 
 file=$(ls -Art /home/${SUDO_USER:-${USER}}/*.csv | tail -n 1)
@@ -13,13 +17,4 @@ cut --complement -f 1-2 -d, $file >> "./results/test_results.csv"
 #mv /home/${SUDO_USER:-${USER}}/*.csv "./results/"
 mv $file "./results/"
 echo "Result moved to results/"
-
-#remove unnecessary details
-#for f in "./results/"
-#do
-#  echo $f
-#  sed -i -e '1,24d' $f
-#  cut --complement -f 1-2 -d, $f >> results.csv
-#done
-
 

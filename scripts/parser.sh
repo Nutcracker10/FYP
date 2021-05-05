826477f76a88cb31a55b50f77947b198beb9338f
James Kirwan <james.kirwan1@ucdconnect.ie>
2021-05-05 21:29:17 +0100

    fixed typo

--- scripts/parser.sh
+++ scripts/parser.sh
@@ -11,7 +11,7 @@ phoronix-test-suite result-file-to-csv $result
 file=$(ls -Art /home/${SUDO_USER:-${USER}}/*.csv | tail -n 1)
 
 #Pre-process the csv
-sed -i -e '1,24d' $file
+sed -i -e '1,23d' $file
 cut --complement -f 1-2 -d, $file >> "./results/temp_results.csv"
 sed -i -z 's/\n/,/g;s/,$/\n/' "./results/temp_results.csv"
 echo $(cat "./results/temp_results.csv") >> "./results/test_results.csv"
